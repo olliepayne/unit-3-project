6f6d8a0a78465aec3a7d78a15f9051410aded9e6
olliepayne <olliepayne@me.com>
2021-01-18 11:39:09 -0700

    stub directory structure & basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Route
+        exact path="/"
+      />
     </div>
   );
 }
