3205649fefff7b2a883df528985cbdba5ec020fa
olliepayne <olliepayne@me.com>
2021-01-18 16:50:42 -0700

    backup

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const userRouter = require('./routes/userRouter')
 
 const app = express()
 
+// - - - middleware - - -
+app.use(express.json())
+app.use(cors())
+
+// - - - routers - - -
+app.use('/users', userRouter)
+
+// - - - start the server - - -
 const port = process.env.PORT || 3001
 app.listen(port, () => console.log(`Server connected, listening on port ${port}`))
