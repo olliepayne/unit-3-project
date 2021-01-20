cee9b50b8f326ceffebecea354b62aa7124e573d
olliepayne <olliepayne@me.com>
2021-01-19 18:04:35 -0700

    backup

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
+import Login from '../../pages/Login/Login'
+import Landing from '../../pages/Landing/Landing'
 import RouteList from '../RouteList/RouteList'
 import AddRoute from '../AddRoute/AddRoute'
 import * as authService from '../../services/authService'
@@ -17,6 +19,7 @@ function App() {
       <Navbar />
       <Route
         exact path="/"
+        render={() => <Landing />}
       />
       <Route
         exact path="/routes"
