696ad232a23cb65fcd4ee19ef236a03ce13fe004
olliepayne <olliepayne@me.com>
2021-01-22 18:02:16 -0700

    tweak signup page

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -5,26 +5,45 @@ const Signup = props => {
 
   const [formData, setFormData] = useState({})
 
+  const [passwordMatch, setPasswordMatch] = useState(false)
+
   const handleChange = e => {
+    if(e.target.name === 'passwordCheck') {
+      const passwordCheckVal = e.target.value
+      if(formData['password'] !== null) {
+        if(passwordCheckVal === formData.password) {
+          setPasswordMatch(true)
+        } else {
+          setPasswordMatch(false)
+        }
+      }
+    }
+
     let newData = formData
     newData[e.target.name] = e.target.value
     setFormData(newData)
   }
 
   const handleSubmit = e => {
-    e.preventDefault()
-    handleSignup(formData)
+    if(passwordMatch) {
+      e.preventDefault()
+      handleSignup(formData)
+    }
   }
 
   return (
     <div className="signup-page">
       <form className="login-form" onSubmit={e => handleSubmit(e)}>
+        <label>Username</label>
+        <input name="username" type="text" onChange={e => handleChange(e)} />
+        <br />
         <label>Email Address</label>
         <input name="email" type="text" onChange={e => handleChange(e)} />
         <br />
         <label>Password</label>
         <input name="password" type="password" onChange={e => handleChange(e)} />
         <br />
+        <label>Confirm Password</label>
         <input name="passwordCheck" type="password" onChange={e => handleChange(e)} />
         <br />
         <button>Submit</button>
