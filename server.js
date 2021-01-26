00f4eab614f8be5e47862e0d4c442cd74ebba363
olliepayne <olliepayne@me.com>
2021-01-26 11:36:06 -0700

    backup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path');
+const favicon = require('serve-favicon');
 require('dotenv').config()
 require('./config/database')
 
@@ -13,6 +14,9 @@ const app = express()
 // - - - middleware - - -
 app.use(express.json())
 app.use(cors())
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // - - - routers - - -
 app.use('/api/auth', authRouter)
